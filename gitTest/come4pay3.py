ae3975870e8a29b4153e2d0fd641286bd03fab47
koijicool <suthongchai.su@gmail.com>
2021-02-15 13:54:02 +0100

    https://github.com/Koijicool/gitTest.git

--- gitTest/come4pay3.py
+++ gitTest/come4pay3.py
@@ -1,11 +1,11 @@
 def comeXpayY(come_x, pay_y, per_head, pax):
     """
-    มา x จ่าย y เช่น มา 4 จ่าย 3
-    :param come_x: มา
-    :param pay_y: จ่าย
-    :param per_head: ค่าอาหารต่อหัว
-    :param pax: จำนวนลูกค้าในโต๊ะ
-    :return: ค่าอาหาร
+   
+    :param come_x: 
+    :param pay_y: 
+    :param per_head: 
+    :param pax: 
+    :return: 
     """
     return (pax // come_x) * (pay_y * per_head) + (pax % come_x) * per_head
 
